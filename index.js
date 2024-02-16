3d832aac89be27c10061131b900434b89eaa0643
Kola771 <koladeaboudou@gmail.com>
2024-02-16 10:45:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("hello world ! 👌🤣🤣")
+
+const http = require('http');
+const port = 9000;
+const hostname = '127.0.0.1';
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 200;
+    res.end('<h1>Hello World ! 👌</h1>');
+})
+
+// server.listen(port, hostname, () => {
+//     console.log("Server prêt sur", hostname, ":", port)
+// });
+
+server.listen(port, hostname, () => {
+    console.log(`Server prêt sur le http://${hostname}:${port}`)
+});
